4bacee0b5d28c9154985cca22660bbfaa268bcf7
Lurppino <luka.ulmanen@gmail.com>
2025-09-01 10:13:46 +0300

    goblin.cs and dia

---
+++ Goblin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ritaripeli
+{
+    internal class Goblin : Hirviö
+    {
+        public Goblin()
+        {
+            Nimi = "Goblin";
+            Osumapisteet = 5;
+        }
+
+        public override int AnnaVahinko() => 2;
+        public override void OtaVahinkoa(int maara) => Osumapisteet -= maara;
+    }
+}
