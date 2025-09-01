18d151be01b22f2bb4c5229b18a8e59062b73694
Lurppino <luka.ulmanen@gmail.com>
2025-09-01 11:44:47 +0300

    create ruokakauppa.cs

---
+++ RuokaKauppa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ritaripeli
+{
+    internal class RuokaKauppa : IKauppa
+    {
+        private List<TavaraJaHinta> tuotteet;
+
+        public RuokaKauppa()
+        {
+            tuotteet = new List<TavaraJaHinta>()
+            {
+                new TavaraJaHinta(new Ruoka("Leipä", 5, 3), 5),
+                new TavaraJaHinta(new Ruoka("Keitto", 10, 7), 10),
+                new TavaraJaHinta(new Ruoka("Kultainen Omena", 25, 20), 25)
+            };
+        }
+
+        public List<TavaraJaHinta> ListaaTavarat()
+        {
+            return tuotteet;
+        }
+
+        public Tavara? OstaTavara(int valittuTavara, Lompakko rahapussi)
+        {
+            if (valittuTavara < 0 || valittuTavara >= tuotteet.Count)
+            {
+                Console.WriteLine("Virheellinen valinta!");
+                return null;
+            }
+
+            TavaraJaHinta valinta = tuotteet[valittuTavara];
+
+            int maksettu = rahapussi.OtaRahaa(valinta.Hinta);
+            if (maksettu == valinta.Hinta)
+            {
+                Console.WriteLine($"Ostit: {valinta.Esine.Nimi} ({valinta.Hinta} kultaa)");
+                return valinta.Esine;
+            }
+            else
+            {
+                Console.WriteLine("Sinulla ei ole tarpeeksi kultaa!");
+                return null;
+            }
+        }
+    }
+}
