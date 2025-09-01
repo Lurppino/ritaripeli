8a780052cabc9fdedef521347f9c98b9e2def32f
Lurppino <luka.ulmanen@gmail.com>
2025-09-01 09:27:06 +0300

    made weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,17 @@
+﻿namespace ritaripeli
+{
+    internal class Weapon : Tavara
+    {
+        public int Vahinko { get; private set; } 
+
+        public Weapon(string nimi, int arvo, int vahinko) : base(nimi, arvo)
+        {
+            Vahinko = vahinko;
+        }
+
+        public override void Kayta(Ritari ritari)
+        {
+            Console.WriteLine($"{ritari.Nimi} equips {Nimi} with {Vahinko} damage.");
+        }
+    }
+}
