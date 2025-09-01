04320ed3c94dc3df41d0aa7d4941b6e4c61f733c
Lurppino <luka.ulmanen@gmail.com>
2025-09-01 09:43:30 +0300

    arrow and arrowstore.cs

---
+++ Nuoli.cs
@@ -0,0 +1,17 @@
+﻿namespace ritaripeli
+{
+    internal class Arrow : Tavara
+    {
+        public int Damage { get; private set; }
+
+        public Arrow(string nimi, int arvo, int damage) : base(nimi, arvo)
+        {
+            Damage = damage;
+        }
+
+        public override void Kayta(Ritari ritari)
+        {
+            Console.WriteLine($"{ritari.Nimi} shoots a {Nimi} arrow for {Damage} damage!");
+        }
+    }
+}
---
+++ NuoliKauppa.cs
@@ -0,0 +1,43 @@
+﻿using ritaripeli;
+
+internal class NuoliKauppa : IKauppa
+{
+    private List<TavaraJaHinta> tuotteet;
+
+    public NuoliKauppa()
+    {
+        tuotteet = new List<TavaraJaHinta>()
+        {
+            new TavaraJaHinta(new Arrow("Puunuoli", 5, 10), 5),
+            new TavaraJaHinta(new Arrow("Teräsnuoli", 15, 25), 15)
+        };
+    }
+
+    public List<TavaraJaHinta> ListaaTavarat()
+    {
+        return tuotteet;
+    }
+
+    public Tavara? OstaTavara(int valittuTavara, Lompakko rahapussi)
+    {
+        if (valittuTavara < 0 || valittuTavara >= tuotteet.Count)
+        {
+            Console.WriteLine("Virheellinen valinta!");
+            return null;
+        }
+
+        TavaraJaHinta valinta = tuotteet[valittuTavara];
+
+        int maksettu = rahapussi.OtaRahaa(valinta.Hinta);
+        if (maksettu == valinta.Hinta)
+        {
+            Console.WriteLine($"Ostit: {valinta.Esine.Nimi} ({valinta.Hinta} kultaa)");
+            return valinta.Esine;
+        }
+        else
+        {
+            Console.WriteLine("Sinulla ei ole tarpeeksi kultaa!");
+            return null;
+        }
+    }
+}
